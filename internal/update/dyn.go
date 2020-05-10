4ce9c17964f4439dd441442af04a47396aa556fa
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-05-10 23:32:58 +0000

    Using simpler url for dyndns api

--- internal/update/dyn.go
+++ internal/update/dyn.go
@@ -1,54 +1,46 @@
 package update
 
 import (
-	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
+	"net/url"
 
-	"github.com/qdm12/ddns-updater/internal/constants"
 	"github.com/qdm12/golibs/network"
 )
 
-// See https://help.dyn.com/update-a-record-api/
-// Token obtained using https://help.dyn.com/session-log-in/
-func updateDyn(client network.Client, zone, fqdn, recordID, token string, ip net.IP) (err error) {
-	if ip == nil {
-		return fmt.Errorf("IP address was not given to updater")
+func updateDyn(client network.Client, username, password, domain, host string, ipv4, ipv6 net.IP) (err error) {
+	u := url.URL{
+		Scheme: "https",
+		User:   url.UserPassword(username, password),
+		Host:   "members.dyndns.org",
+		Path:   "/v3/update",
 	}
-	url := fmt.Sprintf("%s/REST/ARecord/%s/%s/%s/", constants.DynURL, zone, fqdn, recordID)
-	var body struct {
-		Rdata struct {
-			Address string `json:"address"`
-		} `json:"rdata"`
-		TTL string `json:"ttl"`
+	var values url.Values
+	switch host {
+	case "@":
+		values.Set("hostname", domain)
+	default:
+		values.Set("hostname", fmt.Sprintf("%s.%s", host, domain))
 	}
-	body.TTL = "0"
-	body.Rdata.Address = ip.String()
-	r, err := http.NewRequest(http.MethodPut, url, nil)
+	if ipv4 != nil {
+		values.Set("myip", ipv4.String())
+	}
+	if ipv6 != nil {
+		values.Add("myip", ipv6.String())
+	}
+	u.RawQuery = values.Encode()
+	r, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
 		return err
 	}
 	r.Header.Set("User-Agent", "DDNS-Updater quentin.mcgaw@gmail.com")
-	status, content, err := client.DoHTTPRequest(r)
+	status, _, err := client.DoHTTPRequest(r)
 	if err != nil {
 		return err
 	}
-	var response struct {
-		Rdata struct {
-			Address string `json:"address"`
-		} `json:"rdata"`
-	}
 	if status != http.StatusOK {
 		return fmt.Errorf("HTTP status %d", status)
-	} else if err := json.Unmarshal(content, &response); err != nil {
-		return err
-	}
-	newIP := net.ParseIP(response.Rdata.Address)
-	if newIP == nil {
-		return fmt.Errorf("IP address received %q is malformed", response.Rdata.Address)
-	} else if ip != nil && !ip.Equal(newIP) {
-		return fmt.Errorf("new IP address %s is not %s", newIP.String(), ip.String())
 	}
 	return nil
 }

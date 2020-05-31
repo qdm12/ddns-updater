0a265176cf33ef612e41820a833ac6897782be73
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-05-31 00:34:31 +0000

    Add noip echo service and improve documentation

--- internal/constants/ipmethod.go
+++ internal/constants/ipmethod.go
@@ -53,5 +53,25 @@ func IPMethods() []models.IPMethod {
 			IPv4: true,
 			IPv6: true,
 		},
+		{
+			Name: "noip4",
+			URL:  "http://ip1.dynupdate.no-ip.com",
+			IPv4: true,
+		},
+		{
+			Name: "noip6",
+			URL:  "http://ip1.dynupdate6.no-ip.com",
+			IPv6: true,
+		},
+		{
+			Name: "noip8245_4",
+			URL:  "http://ip1.dynupdate.no-ip.com:8245",
+			IPv4: true,
+		},
+		{
+			Name: "noip8245_6",
+			URL:  "http://ip1.dynupdate6.no-ip.com:8245",
+			IPv6: true,
+		},
 	}
 }

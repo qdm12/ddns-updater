8e97f13ff261a5df117da89b9ab59f50b2355cfc
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-03-13 22:40:31 +0000

    Add HTTP fetch unit test for context canceled

--- pkg/publicip/http/fetch_test.go
+++ pkg/publicip/http/fetch_test.go
@@ -18,47 +18,64 @@ func Test_fetch(t *testing.T) {
 
 	errDummy := errors.New("dummy")
 
+	canceledCtx, cancel := context.WithCancel(context.Background())
+	cancel()
+
 	tests := map[string]struct {
+		ctx         context.Context
 		url         string
 		httpContent []byte
 		httpErr     error
 		publicIP    net.IP
 		err         error
 	}{
+		"canceled context": {
+			ctx: canceledCtx,
+			url: "https://opendns.com/ip",
+			err: errors.New(`Get "https://opendns.com/ip": context canceled`),
+		},
 		"http error": {
+			ctx:     context.Background(),
 			url:     "https://opendns.com/ip",
 			httpErr: errDummy,
 			err:     errors.New(`Get "https://opendns.com/ip": dummy`),
 		},
 		"empty response": {
+			ctx: context.Background(),
 			url: "https://opendns.com/ip",
 			err: errors.New(`no IP address found`),
 		},
 		"no IP in response": {
+			ctx:         context.Background(),
 			url:         "https://opendns.com/ip",
 			httpContent: []byte(`abc def`),
 			err:         errors.New(`no IP address found`),
 		},
 		"ipv4 and ipv6": {
+			ctx:         context.Background(),
 			url:         "https://opendns.com/ip",
 			httpContent: []byte(`1.67.201.251 ::0`),
 			err:         errors.New(`too many IP addresses: found 1 IPv4 addresses and 1 IPv6 addresses, instead of a single one`), //nolint:lll
 		},
 		"too many ipv4": {
+			ctx:         context.Background(),
 			url:         "https://opendns.com/ip",
 			httpContent: []byte(`1.67.201.251 1.67.201.251`),
 			err:         errors.New(`too many IP addresses: found 2 IP addresses instead of a single one`),
 		},
 		"too many ipv6": {
+			ctx:         context.Background(),
 			url:         "https://opendns.com/ip",
 			httpContent: []byte(`::0 ::0`),
 			err:         errors.New(`too many IP addresses: found 2 IP addresses instead of a single one`),
 		},
 		"ipv4": {
+			ctx:         context.Background(),
 			httpContent: []byte("blabla 1.67.201.251 25.35.55 sds"),
 			publicIP:    net.IP{1, 67, 201, 251},
 		},
 		"ipv6": {
+			ctx:         context.Background(),
 			httpContent: []byte("blabla ad07:e846:51ac:6cd0:0000:0000:0000:0000 sds"),
 			publicIP:    net.IP{0xad, 0x7, 0xe8, 0x46, 0x51, 0xac, 0x6c, 0xd0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
 		},
@@ -68,12 +85,12 @@ func Test_fetch(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			ctx := context.Background()
-
 			client := &http.Client{
 				Transport: roundTripFunc(func(r *http.Request) (*http.Response, error) {
 					assert.Equal(t, tc.url, r.URL.String())
-					if tc.httpErr != nil {
+					if err := r.Context().Err(); err != nil {
+						return nil, err
+					} else if tc.httpErr != nil {
 						return nil, tc.httpErr
 					}
 					return &http.Response{
@@ -83,7 +100,7 @@ func Test_fetch(t *testing.T) {
 				}),
 			}
 
-			publicIP, err := fetch(ctx, client, tc.url)
+			publicIP, err := fetch(tc.ctx, client, tc.url)
 
 			if tc.err != nil {
 				require.Error(t, err)

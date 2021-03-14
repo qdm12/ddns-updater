91015554e5c0e58e69d82ee061650c5eac1c6b61
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-03-14 02:19:12 +0000

    Add integration tests

---
+++ pkg/publicip/dns/integration_test.go
@@ -0,0 +1,30 @@
+package dns
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_integration(t *testing.T) {
+	t.Parallel()
+
+	fetcher, err := New(SetProviders(Google))
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	publicIP1, err := fetcher.IP4(ctx)
+	require.NoError(t, err)
+	assert.NotNil(t, publicIP1)
+
+	publicIP2, err := fetcher.IP4(ctx)
+	require.NoError(t, err)
+	assert.NotNil(t, publicIP2)
+
+	assert.Equal(t, publicIP1, publicIP2)
+
+	t.Logf("Public IP is %s", publicIP1)
+}
---
+++ pkg/publicip/http/integration_test.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_integration(t *testing.T) {
+	t.Parallel()
+
+	client := &http.Client{}
+
+	fetcher, err := New(client, SetProvidersIP(Opendns))
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	publicIP1, err := fetcher.IP4(ctx)
+	require.NoError(t, err)
+	assert.NotNil(t, publicIP1)
+
+	publicIP2, err := fetcher.IP4(ctx)
+	require.NoError(t, err)
+	assert.NotNil(t, publicIP2)
+
+	assert.Equal(t, publicIP1, publicIP2)
+
+	t.Logf("Public IP is %s", publicIP1)
+}

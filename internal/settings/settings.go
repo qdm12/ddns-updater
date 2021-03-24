532a09662999b3a63bc14959468bb1d37bb19316
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-03-24 02:35:38 +0000

    Settings mock

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -15,6 +15,8 @@ import (
 	"github.com/qdm12/ddns-updater/pkg/publicip/ipversion"
 )
 
+//go:generate mockgen -destination=mock_$GOPACKAGE/$GOFILE . Settings
+
 type Settings interface {
 	String() string
 	Domain() string

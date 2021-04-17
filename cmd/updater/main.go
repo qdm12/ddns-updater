864a69668082b4e7badfbd29a9ead67fb69766e4
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-04-17 01:20:42 +0000

    Maintenance: upgrade golibs

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -73,13 +73,13 @@ func _main(ctx context.Context, timeNow func() time.Time) int {
 	fmt.Println(splash.Splash(buildInfo))
 
 	// Setup logger
-	paramsReader := params.NewReader(logging.New(logging.StdLog)) // use a temporary logger
+	paramsReader := params.NewReader(logging.New(logging.Settings{})) // use a temporary logger
 	logLevel, logCaller, err := paramsReader.LoggerConfig()
 	if err != nil {
 		fmt.Println(err)
 		return 1
 	}
-	logger := logging.New(logging.StdLog, logging.SetLevel(logLevel), logging.SetCaller(logCaller))
+	logger := logging.NewParent(logging.Settings{Level: logLevel, Caller: logCaller})
 	paramsReader = params.NewReader(logger)
 
 	notify, err := setupGotify(paramsReader, logger)
@@ -172,20 +172,21 @@ func _main(ctx context.Context, timeNow func() time.Time) int {
 	const healthServerAddr = "127.0.0.1:9999"
 	isHealthy := health.MakeIsHealthy(db, net.LookupIP, logger)
 	healthServer := health.NewServer(healthServerAddr,
-		logger.NewChild(logging.SetPrefix("healthcheck server: ")),
+		logger.NewChild(logging.Settings{Prefix: "healthcheck server: "}),
 		isHealthy)
 	wg.Add(1)
 	go healthServer.Run(ctx, wg)
 
 	address := fmt.Sprintf("0.0.0.0:%d", p.listeningPort)
 	uiDir := p.dir + "/ui"
-	server := server.New(ctx, address, p.rootURL, uiDir, db, logger.NewChild(logging.SetPrefix("http server: ")), runner)
+	serverLogger := logger.NewChild(logging.Settings{Prefix: "http server: "})
+	server := server.New(ctx, address, p.rootURL, uiDir, db, serverLogger, runner)
 	wg.Add(1)
 	go server.Run(ctx, wg)
 	notify(1, fmt.Sprintf("Launched with %d records to watch", len(records)))
 
 	go backupRunLoop(ctx, p.backupPeriod, p.dir, p.backupDirectory,
-		logger.NewChild(logging.SetPrefix("backup: ")), timeNow)
+		logger.NewChild(logging.Settings{Prefix: "backup: "}), timeNow)
 
 	osSignals := make(chan os.Signal, 1)
 	signal.Notify(osSignals,

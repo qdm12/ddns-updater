c39df2796f1d4d2ddc030522b4e3f0e701818bb3
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-06-07 09:17:38 +0000

    chore(ci): add mocks check step

--- pkg/publicip/dns/mock_dns/client.go
+++ pkg/publicip/dns/mock_dns/client.go
@@ -6,36 +6,37 @@ package mock_dns
 
 import (
 	context "context"
-	gomock "github.com/golang/mock/gomock"
-	dns "github.com/miekg/dns"
 	reflect "reflect"
 	time "time"
+
+	gomock "github.com/golang/mock/gomock"
+	dns "github.com/miekg/dns"
 )
 
-// MockClient is a mock of Client interface
+// MockClient is a mock of Client interface.
 type MockClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockClientMockRecorder
 }
 
-// MockClientMockRecorder is the mock recorder for MockClient
+// MockClientMockRecorder is the mock recorder for MockClient.
 type MockClientMockRecorder struct {
 	mock *MockClient
 }
 
-// NewMockClient creates a new mock instance
+// NewMockClient creates a new mock instance.
 func NewMockClient(ctrl *gomock.Controller) *MockClient {
 	mock := &MockClient{ctrl: ctrl}
 	mock.recorder = &MockClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockClient) EXPECT() *MockClientMockRecorder {
 	return m.recorder
 }
 
-// ExchangeContext mocks base method
+// ExchangeContext mocks base method.
 func (m *MockClient) ExchangeContext(arg0 context.Context, arg1 *dns.Msg, arg2 string) (*dns.Msg, time.Duration, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ExchangeContext", arg0, arg1, arg2)
@@ -45,7 +46,7 @@ func (m *MockClient) ExchangeContext(arg0 context.Context, arg1 *dns.Msg, arg2 s
 	return ret0, ret1, ret2
 }
 
-// ExchangeContext indicates an expected call of ExchangeContext
+// ExchangeContext indicates an expected call of ExchangeContext.
 func (mr *MockClientMockRecorder) ExchangeContext(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ExchangeContext", reflect.TypeOf((*MockClient)(nil).ExchangeContext), arg0, arg1, arg2)

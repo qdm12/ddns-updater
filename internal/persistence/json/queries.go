1e8f87659ef9b819b1d5e66a9b45f0a0502d1b49
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-01-15 09:32:01 +0000

    fix(internal/persistence/json): get events by IP version

--- internal/persistence/json/queries.go
+++ internal/persistence/json/queries.go
@@ -1,10 +1,12 @@
 package json
 
 import (
+	"fmt"
 	"net/netip"
 	"time"
 
 	"github.com/qdm12/ddns-updater/internal/models"
+	"github.com/qdm12/ddns-updater/pkg/publicip/ipversion"
 )
 
 // StoreNewIP stores a new IP address for a certain domain and host.
@@ -37,15 +39,37 @@ func (db *Database) StoreNewIP(domain, host string, ip netip.Addr, t time.Time) 
 	return db.write()
 }
 
-// GetEvents gets all the IP addresses history for a certain domain and host, in the order
-// from oldest to newest.
-func (db *Database) GetEvents(domain, host string) (events []models.HistoryEvent, err error) {
+// GetEvents gets all the IP addresses history for a certain domain, host and
+// IP version, in the order from oldest to newest.
+func (db *Database) GetEvents(domain, host string,
+	ipVersion ipversion.IPVersion) (events []models.HistoryEvent, err error) {
 	db.RLock()
 	defer db.RUnlock()
 	for _, record := range db.data.Records {
 		if record.Domain == domain && record.Host == host {
-			return append(events, record.Events...), nil
+			return filterEvents(events, ipVersion), nil
 		}
 	}
 	return nil, nil
+}
+
+func filterEvents(events []models.HistoryEvent, ipVersion ipversion.IPVersion) (filteredEvents []models.HistoryEvent) {
+	filteredEvents = make([]models.HistoryEvent, 0, len(events))
+	for _, event := range events {
+		switch ipVersion {
+		case ipversion.IP4:
+			if event.IP.Is4() {
+				filteredEvents = append(filteredEvents, event)
+			}
+		case ipversion.IP6:
+			if event.IP.Is6() {
+				filteredEvents = append(filteredEvents, event)
+			}
+		case ipversion.IP4or6:
+			filteredEvents = append(filteredEvents, event)
+		default:
+			panic(fmt.Sprintf("IP version %v is not supported", ipVersion))
+		}
+	}
+	return filteredEvents
 }

46054fb631b21a8e9d34672ae3b3f36f8452ea3a
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-01-18 17:01:14 +0000

    chore(porkbun): split API specific code to api.go

---
+++ internal/provider/providers/porkbun/api.go
@@ -0,0 +1,171 @@
+package porkbun
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/qdm12/ddns-updater/internal/provider/errors"
+)
+
+// See https://porkbun.com/api/json/v3/documentation#DNS%20Retrieve%20Records%20by%20Domain,%20Subdomain%20and%20Type
+func (p *Provider) getRecordIDs(ctx context.Context, client *http.Client, recordType string) (
+	recordIDs []string, err error) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "porkbun.com",
+		Path:   "/api/json/v3/dns/retrieveByNameType/" + p.domain + "/" + recordType + "/",
+	}
+	if p.host != "@" {
+		u.Path += p.host
+	}
+
+	postRecordsParams := struct {
+		SecretAPIKey string `json:"secretapikey"`
+		APIKey       string `json:"apikey"`
+	}{
+		SecretAPIKey: p.secretAPIKey,
+		APIKey:       p.apiKey,
+	}
+	buffer := bytes.NewBuffer(nil)
+	encoder := json.NewEncoder(buffer)
+	err = encoder.Encode(postRecordsParams)
+	if err != nil {
+		return nil, fmt.Errorf("json encoding request data: %w", err)
+	}
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
+	if err != nil {
+		return nil, fmt.Errorf("creating http request: %w", err)
+	}
+	setHeaders(request)
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("doing http request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%w: %d: %s", errors.ErrHTTPStatusNotValid,
+			response.StatusCode, makeErrorMessage(response.Body))
+	}
+
+	var responseData struct {
+		Records []struct {
+			ID string `json:"id"`
+		} `json:"records"`
+	}
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&responseData)
+	if err != nil {
+		return nil, fmt.Errorf("json decoding response body: %w", err)
+	}
+
+	for _, record := range responseData.Records {
+		recordIDs = append(recordIDs, record.ID)
+	}
+
+	return recordIDs, nil
+}
+
+// See https://porkbun.com/api/json/v3/documentation#DNS%20Create%20Record
+func (p *Provider) createRecord(ctx context.Context, client *http.Client,
+	recordType string, ipStr string) (err error) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "porkbun.com",
+		Path:   "/api/json/v3/dns/create/" + p.domain,
+	}
+	postRecordsParams := struct {
+		SecretAPIKey string `json:"secretapikey"`
+		APIKey       string `json:"apikey"`
+		Content      string `json:"content"`
+		Name         string `json:"name,omitempty"`
+		Type         string `json:"type"`
+		TTL          string `json:"ttl"`
+	}{
+		SecretAPIKey: p.secretAPIKey,
+		APIKey:       p.apiKey,
+		Content:      ipStr,
+		Type:         recordType,
+		Name:         p.host,
+		TTL:          fmt.Sprint(p.ttl),
+	}
+	buffer := bytes.NewBuffer(nil)
+	encoder := json.NewEncoder(buffer)
+	err = encoder.Encode(postRecordsParams)
+	if err != nil {
+		return fmt.Errorf("json encoding request data: %w", err)
+	}
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
+	if err != nil {
+		return fmt.Errorf("creating http request: %w", err)
+	}
+	setHeaders(request)
+
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("doing http request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("%w: %d: %s", errors.ErrHTTPStatusNotValid,
+			response.StatusCode, makeErrorMessage(response.Body))
+	}
+	return nil
+}
+
+// See https://porkbun.com/api/json/v3/documentation#DNS%20Edit%20Record%20by%20Domain%20and%20ID
+func (p *Provider) updateRecord(ctx context.Context, client *http.Client,
+	recordType string, ipStr string, recordID string) (err error) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "porkbun.com",
+		Path:   "/api/json/v3/dns/edit/" + p.domain + "/" + recordID,
+	}
+	postRecordsParams := struct {
+		SecretAPIKey string `json:"secretapikey"`
+		APIKey       string `json:"apikey"`
+		Content      string `json:"content"`
+		Type         string `json:"type"`
+		TTL          string `json:"ttl"`
+		Name         string `json:"name,omitempty"`
+	}{
+		SecretAPIKey: p.secretAPIKey,
+		APIKey:       p.apiKey,
+		Content:      ipStr,
+		Type:         recordType,
+		TTL:          fmt.Sprint(p.ttl),
+		Name:         p.host,
+	}
+	buffer := bytes.NewBuffer(nil)
+	encoder := json.NewEncoder(buffer)
+	err = encoder.Encode(postRecordsParams)
+	if err != nil {
+		return fmt.Errorf("json encoding request data: %w", err)
+	}
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
+	if err != nil {
+		return fmt.Errorf("creating http request: %w", err)
+	}
+	setHeaders(request)
+
+	response, err := client.Do(request)
+	if err != nil {
+		return fmt.Errorf("doing http request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("%w: %d: %s", errors.ErrHTTPStatusNotValid,
+			response.StatusCode, makeErrorMessage(response.Body))
+	}
+	return nil
+}
--- internal/provider/providers/porkbun/provider.go
+++ internal/provider/providers/porkbun/provider.go
@@ -1,13 +1,11 @@
 package porkbun
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/netip"
-	"net/url"
 
 	"github.com/qdm12/ddns-updater/internal/models"
 	"github.com/qdm12/ddns-updater/internal/provider/constants"
@@ -101,162 +99,7 @@ func setHeaders(request *http.Request) {
 	headers.SetAccept(request, "application/json")
 }
 
-func (p *Provider) getRecordIDs(ctx context.Context, client *http.Client, recordType string) (
-	recordIDs []string, err error) {
-	u := url.URL{
-		Scheme: "https",
-		Host:   "porkbun.com",
-		Path:   "/api/json/v3/dns/retrieveByNameType/" + p.domain + "/" + recordType + "/",
-	}
-	if p.host != "@" {
-		u.Path += p.host
-	}
-
-	postRecordsParams := struct {
-		SecretAPIKey string `json:"secretapikey"`
-		APIKey       string `json:"apikey"`
-	}{
-		SecretAPIKey: p.secretAPIKey,
-		APIKey:       p.apiKey,
-	}
-	buffer := bytes.NewBuffer(nil)
-	encoder := json.NewEncoder(buffer)
-	err = encoder.Encode(postRecordsParams)
-	if err != nil {
-		return nil, fmt.Errorf("json encoding request data: %w", err)
-	}
-
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-	if err != nil {
-		return nil, fmt.Errorf("creating http request: %w", err)
-	}
-	setHeaders(request)
-
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("doing http request: %w", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("%w: %d: %s", errors.ErrHTTPStatusNotValid,
-			response.StatusCode, makeErrorMessage(response.Body))
-	}
-
-	var responseData struct {
-		Records []struct {
-			ID string `json:"id"`
-		} `json:"records"`
-	}
-	decoder := json.NewDecoder(response.Body)
-	err = decoder.Decode(&responseData)
-	if err != nil {
-		return nil, fmt.Errorf("json decoding response body: %w", err)
-	}
-
-	for _, record := range responseData.Records {
-		recordIDs = append(recordIDs, record.ID)
-	}
-
-	return recordIDs, nil
-}
-
-func (p *Provider) createRecord(ctx context.Context, client *http.Client,
-	recordType string, ipStr string) (err error) {
-	u := url.URL{
-		Scheme: "https",
-		Host:   "porkbun.com",
-		Path:   "/api/json/v3/dns/create/" + p.domain,
-	}
-	postRecordsParams := struct {
-		SecretAPIKey string `json:"secretapikey"`
-		APIKey       string `json:"apikey"`
-		Content      string `json:"content"`
-		Name         string `json:"name,omitempty"`
-		Type         string `json:"type"`
-		TTL          string `json:"ttl"`
-	}{
-		SecretAPIKey: p.secretAPIKey,
-		APIKey:       p.apiKey,
-		Content:      ipStr,
-		Type:         recordType,
-		Name:         p.host,
-		TTL:          fmt.Sprint(p.ttl),
-	}
-	buffer := bytes.NewBuffer(nil)
-	encoder := json.NewEncoder(buffer)
-	err = encoder.Encode(postRecordsParams)
-	if err != nil {
-		return fmt.Errorf("json encoding request data: %w", err)
-	}
-
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-	if err != nil {
-		return fmt.Errorf("creating http request: %w", err)
-	}
-	setHeaders(request)
-
-	response, err := client.Do(request)
-	if err != nil {
-		return fmt.Errorf("doing http request: %w", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("%w: %d: %s", errors.ErrHTTPStatusNotValid,
-			response.StatusCode, makeErrorMessage(response.Body))
-	}
-	return nil
-}
-
-func (p *Provider) updateRecord(ctx context.Context, client *http.Client,
-	recordType string, ipStr string, recordID string) (err error) {
-	u := url.URL{
-		Scheme: "https",
-		Host:   "porkbun.com",
-		Path:   "/api/json/v3/dns/edit/" + p.domain + "/" + recordID,
-	}
-	postRecordsParams := struct {
-		SecretAPIKey string `json:"secretapikey"`
-		APIKey       string `json:"apikey"`
-		Content      string `json:"content"`
-		Type         string `json:"type"`
-		TTL          string `json:"ttl"`
-		Name         string `json:"name,omitempty"`
-	}{
-		SecretAPIKey: p.secretAPIKey,
-		APIKey:       p.apiKey,
-		Content:      ipStr,
-		Type:         recordType,
-		TTL:          fmt.Sprint(p.ttl),
-		Name:         p.host,
-	}
-	buffer := bytes.NewBuffer(nil)
-	encoder := json.NewEncoder(buffer)
-	err = encoder.Encode(postRecordsParams)
-	if err != nil {
-		return fmt.Errorf("json encoding request data: %w", err)
-	}
-
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), buffer)
-	if err != nil {
-		return fmt.Errorf("creating http request: %w", err)
-	}
-	setHeaders(request)
-
-	response, err := client.Do(request)
-	if err != nil {
-		return fmt.Errorf("doing http request: %w", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("%w: %d: %s", errors.ErrHTTPStatusNotValid,
-			response.StatusCode, makeErrorMessage(response.Body))
-	}
-	return nil
-}
-
+// See https://porkbun.com/api/json/v3/documentation
 func (p *Provider) Update(ctx context.Context, client *http.Client, ip netip.Addr) (newIP netip.Addr, err error) {
 	recordType := constants.A
 	if ip.Is6() {

qdm12/ddns-updater

commit 7ed63a036ef310f24373802b89e2ac946844f2ca

fix(internal/persistence/json): get events by IP version (#514)

internal/persistence/json/queries.go
@@ -1,10 +1,12 @@
 package json
 
 import (
+	"fmt"
 	"net/netip"
 	"time"
 
 	"github.com/qdm12/ddns-updater/internal/models"
+	"github.com/qdm12/ddns-updater/pkg/publicip/ipversion"
 )
 
 // StoreNewIP stores a new IP address for a certain domain and host.
@@ -37,15 +39,37 @@ func (db *Database) StoreNewIP(domain, host string, ip netip.Addr, t time.Time) 
 	return db.write()
 }
 
-// GetEvents gets all the IP addresses history for a certain domain and host, in the order
-// from oldest to newest.
-func (db *Database) GetEvents(domain, host string) (events []models.HistoryEvent, err error) {
+// GetEvents gets all the IP addresses history for a certain domain, host and
+// IP version, in the order from oldest to newest.
+func (db *Database) GetEvents(domain, host string,
+	ipVersion ipversion.IPVersion) (events []models.HistoryEvent, err error) {
 	db.RLock()
 	defer db.RUnlock()
 	for _, record := range db.data.Records {
 		if record.Domain == domain && record.Host == host {
-			return append(events, record.Events...), nil
+			return filterEvents(events, ipVersion), nil
 		}
 	}
 	return nil, nil
+}
+
+func filterEvents(events []models.HistoryEvent, ipVersion ipversion.IPVersion) (filteredEvents []models.HistoryEvent) {
+	filteredEvents = make([]models.HistoryEvent, 0, len(events))
+	for _, event := range events {
+		switch ipVersion {
+		case ipversion.IP4:
+			if event.IP.Is4() {
+				filteredEvents = append(filteredEvents, event)
+			}
+		case ipversion.IP6:
+			if event.IP.Is6() {
+				filteredEvents = append(filteredEvents, event)
+			}
+		case ipversion.IP4or6:
+			filteredEvents = append(filteredEvents, event)
+		default:
+			panic(fmt.Sprintf("IP version %v is not supported", ipVersion))
+		}
+	}
+	return filteredEvents
 }

cmd/updater/main.go
@@ -191,8 +191,10 @@ func _main(ctx context.Context, settingsSource SettingsSource, args []string, lo
 	records := make([]recordslib.Record, len(providers))
 	for i, provider := range providers {
 		logger.Info("Reading history from database: domain " +
-			provider.Domain() + " host " + provider.Host())
-		events, err := persistentDB.GetEvents(provider.Domain(), provider.Host())
+			provider.Domain() + " host " + provider.Host() +
+			" " + provider.IPVersion().String())
+		events, err := persistentDB.GetEvents(provider.Domain(),
+			provider.Host(), provider.IPVersion())
 		if err != nil {
 			shoutrrrClient.Notify(err.Error())
 			return err

commit 74168ad4ab4852e019d6d89a0c3bce3e440d3e2b

hotfix(database): fix historical events reading - Regression introduced in 7ed63a036ef310f24373802b89e2ac946844f2ca (PR #514)

internal/persistence/json/queries.go
@@ -47,7 +47,7 @@ func (db *Database) GetEvents(domain, host string,
 	defer db.RUnlock()
 	for _, record := range db.data.Records {
 		if record.Domain == domain && record.Host == host {
-			return filterEvents(events, ipVersion), nil
+			return filterEvents(record.Events, ipVersion), nil
 		}
 	}
 	return nil, nil
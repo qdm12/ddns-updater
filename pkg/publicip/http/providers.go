5e0e3f47027380d82de677f9783ef9ea5d6dfc0c
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-02-12 14:27:57 +0000

    feat(publicip/http): add multiple providers for all IP versions - icanhazip - ident - nnev - wtfismyip

--- pkg/publicip/http/providers.go
+++ pkg/publicip/http/providers.go
@@ -12,12 +12,16 @@ import (
 type Provider string
 
 const (
-	Google   Provider = "google"
-	Ifconfig Provider = "ifconfig"
-	Ipify    Provider = "ipify"
-	Ipinfo   Provider = "ipinfo"
-	Spdyn    Provider = "spdyn"
-	Ipleak   Provider = "ipleak"
+	Google    Provider = "google"
+	Ifconfig  Provider = "ifconfig"
+	Ipify     Provider = "ipify"
+	Ipinfo    Provider = "ipinfo"
+	Spdyn     Provider = "spdyn"
+	Ipleak    Provider = "ipleak"
+	Icanhazip Provider = "icanhazip"
+	Ident     Provider = "ident"
+	Nnev      Provider = "nnev"
+	Wtfismyip Provider = "wtfismyip"
 )
 
 func ListProviders() []Provider {
@@ -28,6 +32,10 @@ func ListProviders() []Provider {
 		Ipinfo,
 		Spdyn,
 		Ipleak,
+		Icanhazip,
+		Ident,
+		Nnev,
+		Wtfismyip,
 	}
 }
 
@@ -72,6 +80,14 @@ func (provider Provider) url(version ipversion.IPVersion) (url string, ok bool) 
 			url = "https://api.ipify.org"
 		case Ipleak:
 			url = "https://ipv4.ipleak.net/json"
+		case Icanhazip:
+			url = "https://ipv4.icanhazip.com"
+		case Ident:
+			url = "https://v4.ident.me"
+		case Nnev:
+			url = "https://ip4.nnev.de"
+		case Wtfismyip:
+			url = "https://ipv4.wtfismyip.com/text"
 		}
 
 	case ipversion.IP6:
@@ -80,6 +96,14 @@ func (provider Provider) url(version ipversion.IPVersion) (url string, ok bool) 
 			url = "https://api6.ipify.org"
 		case Ipleak:
 			url = "https://ipv6.ipleak.net/json"
+		case Icanhazip:
+			url = "https://ipv6.icanhazip.com"
+		case Ident:
+			url = "https://v6.ident.me"
+		case Nnev:
+			url = "https://ip6.nnev.de"
+		case Wtfismyip:
+			url = "https://ipv6.wtfismyip.com/text"
 		}
 
 	case ipversion.IP4or6:
@@ -96,6 +120,14 @@ func (provider Provider) url(version ipversion.IPVersion) (url string, ok bool) 
 			url = "https://checkip.spdyn.de"
 		case Ipleak:
 			url = "https://ipleak.net/json"
+		case Icanhazip:
+			url = "https://icanhazip.com"
+		case Ident:
+			url = "https://ident.me"
+		case Nnev:
+			url = "https://ip.nnev.de"
+		case Wtfismyip:
+			url = "https://wtfismyip.com/text"
 		}
 	}
 
--- pkg/publicip/http/providers_test.go
+++ pkg/publicip/http/providers_test.go
@@ -18,16 +18,17 @@ func Test_ListProvidersForVersion(t *testing.T) {
 		providers []Provider
 	}{
 		"ip4or6": {
-			version:   ipversion.IP4or6,
-			providers: []Provider{Google, Ifconfig, Ipify, Ipinfo, Spdyn, Ipleak},
+			version: ipversion.IP4or6,
+			providers: []Provider{Google, Ifconfig, Ipify, Ipinfo, Spdyn, Ipleak,
+				Icanhazip, Ident, Nnev, Wtfismyip},
 		},
 		"ip4": {
 			version:   ipversion.IP4,
-			providers: []Provider{Ipify, Ipleak},
+			providers: []Provider{Ipify, Ipleak, Icanhazip, Ident, Nnev, Wtfismyip},
 		},
 		"ip6": {
 			version:   ipversion.IP6,
-			providers: []Provider{Ipify, Ipleak},
+			providers: []Provider{Ipify, Ipleak, Icanhazip, Ident, Nnev, Wtfismyip},
 		},
 	}
 

6a6b1a8ebbaf228f3454d6a1f4a2a61addea6c0d
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-02-13 10:42:07 +0000

    feat(pkg/publicip/info): add ip2location.io provider

--- pkg/publicip/info/provider.go
+++ pkg/publicip/info/provider.go
@@ -11,12 +11,14 @@ import (
 type Provider string
 
 const (
-	Ipinfo Provider = "ipinfo"
+	Ipinfo      Provider = "ipinfo"
+	IP2Location Provider = "ip2location"
 )
 
 func ListProviders() []Provider {
 	return []Provider{
 		Ipinfo,
+		IP2Location,
 	}
 }
 
@@ -39,6 +41,8 @@ func newProvider(providerName Provider, client *http.Client) provider { //nolint
 	switch providerName {
 	case Ipinfo:
 		return newIpinfo(client)
+	case IP2Location:
+		return newIP2Location(client)
 	default:
 		panic(fmt.Sprintf("provider %s not implemented", providerName))
 	}

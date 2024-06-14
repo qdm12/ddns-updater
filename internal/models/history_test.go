d3ec047483a9e4555b89757b4454c8d55803cebf
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-06-14 08:49:37 +0000

    fix(ui): list IPs in reverse chronological order - Fix #730

--- internal/models/history_test.go
+++ internal/models/history_test.go
@@ -1,11 +1,57 @@
 package models
 
 import (
+	"net/netip"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_GetPreviousIPs(t *testing.T) {
+	t.Parallel()
+	testCases := map[string]struct {
+		h           History
+		previousIPs []netip.Addr
+	}{
+		"empty_history": {
+			h: History{},
+		},
+		"single_event": {
+			h: History{
+				{IP: netip.MustParseAddr("1.2.3.4")},
+			},
+		},
+		"two_events": {
+			h: History{
+				{IP: netip.MustParseAddr("1.2.3.4")},
+				{IP: netip.MustParseAddr("5.6.7.8")}, // last one
+			},
+			previousIPs: []netip.Addr{
+				netip.MustParseAddr("1.2.3.4"),
+			},
+		},
+		"three_events": {
+			h: History{
+				{IP: netip.MustParseAddr("1.2.3.4")},
+				{IP: netip.MustParseAddr("5.6.7.8")},
+				{IP: netip.MustParseAddr("9.6.7.8")}, // last one
+			},
+			previousIPs: []netip.Addr{
+				netip.MustParseAddr("5.6.7.8"),
+				netip.MustParseAddr("1.2.3.4"),
+			},
+		},
+	}
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			previousIPs := testCase.h.GetPreviousIPs()
+			assert.Equal(t, testCase.previousIPs, previousIPs)
+		})
+	}
+}
 
 func Test_GetDurationSinceSuccess(t *testing.T) {
 	t.Parallel()
